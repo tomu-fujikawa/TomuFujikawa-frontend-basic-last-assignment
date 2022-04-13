1674e2bb56853325a87338052d51d27f313177a4
Tomu Fujikawa <ttomfjkw@gmail.com>
2022-04-13 13:50:09 +0900

    Molecules/NewsAndEventsMoreを作成する

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Item_Newsandeventsmore from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/News-and-events-more" }
+
+export const Newsandeventsmore = () => Item_Newsandeventsmore;
