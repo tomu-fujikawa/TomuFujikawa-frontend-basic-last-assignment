917fbd430da24de834dd676ddaeb3f86423688a7
Tomu Fujikawa <ttomfjkw@gmail.com>
2022-07-18 00:31:43 +0900

    templates-news-and-eventsの作成

---
+++ src/stories/templates-newsandevents.stories.js
@@ -0,0 +1,6 @@
+import Item_NewsAndEvents from "../components/templates/newsandevents/newsandevents.html";
+import "../components/templates/newsandevents/newsandevents.scss";
+
+export default { title: "Templates/NewsAndEvents" };
+
+export const NewsAndEvents = () => Item_NewsAndEvents;
